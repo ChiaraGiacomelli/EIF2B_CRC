495bdf2d0a47c4c6944938c80354e77a29b2054f
ChiaraGiacomelli <101278133+ChiaraGiacomelli@users.noreply.github.com>
2023-11-28 17:54:10 +0000

    Update common_variables.R

--- R_scripts/common_variables.R
+++ R_scripts/common_variables.R
@@ -10,31 +10,27 @@ parent_dir <- '~/data/CGIACOME/AAA_eIF2B/AAA_Collaborative_stuff/Wurzburg_collab
 plot_dir <- paste0(parent_dir,"/plots/PDF_for_paper/")
 tables_dir <- paste0(parent_dir,"/Analysis/tables_for_paper")
 
-#PCA_cols <-c("NTC" = "#fde725", "2B5" = '#20A387FF', "2B1" = '#440154FF', "2B4" = '#33638DFF')
+PCA_cols <-c("NTC" = "#fde725", "2B5" = '#20A387FF', "2B1" = '#440154FF', "2B4" = '#33638DFF')
 
 #set sample names - only 3_c and not 3
 RPF_sample_names <- c('NTC_min_RPF_1', 'NTC_min_RPF_2', 'NTC_min_RPF_3_c', 'NTC_min_RPF_4',
                       '2B1_3A_min_RPF_1', '2B1_3A_min_RPF_2','2B1_3A_min_RPF_3_c', '2B1_3A_min_RPF_4',
                       '2B4_2_min_RPF_1', '2B4_2_min_RPF_2','2B4_2_min_RPF_3_c', '2B4_2_min_RPF_4',
-                      '2B5_min_RPF_1', '2B5_min_RPF_2', '2B5_min_RPF_3_c', '2B5_min_RPF_4',
                       'NTC_plus_RPF_1', 'NTC_plus_RPF_2', 'NTC_plus_RPF_3_c', 'NTC_plus_RPF_4',
                       '2B1_3A_plus_RPF_1', '2B1_3A_plus_RPF_2','2B1_3A_plus_RPF_3_c', '2B1_3A_plus_RPF_4',
-                      '2B4_2_plus_RPF_1', '2B4_2_plus_RPF_2','2B4_2_plus_RPF_3_c', '2B4_2_plus_RPF_4',
-                      '2B5_plus_RPF_1', '2B5_plus_RPF_2', '2B5_plus_RPF_3_c', '2B5_plus_RPF_4')
+                      '2B4_2_plus_RPF_1', '2B4_2_plus_RPF_2','2B4_2_plus_RPF_3_c', '2B4_2_plus_RPF_4')
 
 Total_sample_names <- c('NTC_min_Tot_1', 'NTC_min_Tot_2', 'NTC_min_Tot_3_c', 'NTC_min_Tot_4',
                         '2B1_3A_min_Tot_1', '2B1_3A_min_Tot_2', '2B1_3A_min_Tot_3_c', '2B1_3A_min_Tot_4',
                         '2B4_2_min_Tot_1', '2B4_2_min_Tot_2','2B4_2_min_Tot_3_c', '2B4_2_min_Tot_4',
-                        '2B5_min_Tot_1', '2B5_min_Tot_2', '2B5_min_Tot_3_c', '2B5_min_Tot_4',
                         'NTC_plus_Tot_1', 'NTC_plus_Tot_2', 'NTC_plus_Tot_3_c', 'NTC_plus_Tot_4',
                         '2B1_3A_plus_Tot_1', '2B1_3A_plus_Tot_2','2B1_3A_plus_Tot_3_c', '2B1_3A_plus_Tot_4',
-                        '2B4_2_plus_Tot_1', '2B4_2_plus_Tot_2','2B4_2_plus_Tot_3_c', '2B4_2_plus_Tot_4',
-                        '2B5_plus_Tot_1', '2B5_plus_Tot_2', '2B5_plus_Tot_3_c', '2B5_plus_Tot_4')
+                        '2B4_2_plus_Tot_1', '2B4_2_plus_Tot_2','2B4_2_plus_Tot_3_c', '2B4_2_plus_Tot_4')
 
 RPF_sample_info <- data.frame(sample = RPF_sample_names,
-                              condition = factor(c(rep("min", 16), rep("plus", 16))),
-                              replicate = factor(rep(c("1", "2", "3", "4"), 8)),
-                              shRNA = c(rep("NTC",4),rep("2B1",4),rep("2B4",4),rep("2B5",4), rep("NTC",4),rep("2B1",4),rep("2B4",4),rep("2B5",4)))
+                              condition = factor(c(rep("min", 12), rep("plus", 12))),
+                              replicate = factor(rep(c("1", "2", "3", "4"), 6)),
+                              shRNA = c(rep("NTC",4),rep("2B1",4),rep("2B4",4), rep("NTC",4),rep("2B1",4),rep("2B4",4)))
 
 #set read lengths used for library QC
 lengths <- 25:35
